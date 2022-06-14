18ee0916dcdf5a77cd6793ac7ca13a045f7572b4
Tvisha Agarwal <tvisha.agarwal115@gmail.com>
2022-06-14 12:24:33 -0500

    Initial Commit

---
+++ quizA/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  quizA
+//
+//  Created by Tvisha Agarwal on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func roseWrongOne(_ sender: Any) {
+        var textPlace = "Wrong!"
+        if let newTitle = textPlace.text{
+            label.text = newTitle
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
